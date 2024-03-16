ca9deb752d5cb8198963dd4473f05970fd922ef8
Michael Budnick <mdbudnick@users.noreply.github.com>
2024-03-16 13:33:10 -0400

    Center website on the app

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ export default function App() {
   return (
     <WebView
       source={{ uri: 'kuychiproject.com' }}
-      style={{ flex: 1 }}
+      style={{ flex: 1, marginTop: 30 }}
+      injectedJavaScript={`const meta = document.createElement('meta'); meta.setAttribute('content', 'width=device-width, initial-scale=0.5, maximum-scale=0.5, user-scalable=0'); meta.setAttribute('name', 'viewport'); document.getElementsByTagName('head')[0].appendChild(meta); `}
     />
   );
 }
